e3793d52059d3673ac01d0ca9f63e6b9fe895c76
unknown <hsz3855@gmail.com>
2020-12-28 12:47:33 +0900

    제로 풀이

---
+++ BaekJoon/10773번_제로.java
@@ -0,0 +1,43 @@
+﻿import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// 입력
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		int k = Integer.valueOf(bf.readLine()); 
+		
+
+		
+		for(int i=0; i<k; i++) {
+			int num = Integer.valueOf(bf.readLine());
+			if(num == 0) stack.pop();
+			else {
+				stack.push(num);
+			}
+		}
+		
+		int stackSize = stack.size();
+		int sum = 0;
+		for(int i=0; i<stackSize; i++) {
+			sum += stack.pop();
+		}
+		
+		
+		
+		bw.write(sum +"\n");
+		bw.flush();//남아있는 데이터를 모두 출력시킴
+		bw.close();//스트림을 닫음
+		
+		
+	}
+
+}
