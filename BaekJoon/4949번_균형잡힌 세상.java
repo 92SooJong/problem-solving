cb47b946df009b4c60f6f7d163b159db3ea5d109
unknown <hsz3855@gmail.com>
2020-12-28 19:43:34 +0900

    4949번풀이

---
+++ BaekJoon/4949번_균형잡힌 세상.java
@@ -0,0 +1,66 @@
+﻿import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// 입력
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		Stack<String> stack = new Stack<String>();
+		 
+		
+		while(true) {
+			
+			String sentence = bf.readLine();
+			String ans = "yes";
+			if(sentence.equals(".")) break;
+			else {
+				String[] sentenceArray = sentence.split("");
+				stack.clear();
+				for(String s : sentenceArray ) {
+					if(s.equals("(") || s.equals("[")) stack.push(s);
+					else if(s.equals(")") || s.equals("]")) {
+						
+						if(stack.isEmpty()) {
+							ans ="no";
+							break;
+						}
+						else if(s.equals(")") && stack.peek().equals("(")) {
+							stack.pop();
+							ans = "yes";
+						}
+						else if(s.equals("]") && stack.peek().equals("[")) {
+							stack.pop();
+							ans = "yes";
+						}
+						else {
+							ans ="no";
+							break;
+						}
+						
+					}
+					else if(s.equals(".")) break;
+				}
+				
+				if(!stack.isEmpty()) ans ="no";
+				
+
+			}
+			bw.write(ans +"\n");
+			
+		}
+		
+		
+		bw.flush();//남아있는 데이터를 모두 출력시킴
+		bw.close();//스트림을 닫음
+		
+		
+	}
+
+}
