d61d5ff708ddf898da6cdec253059156361665fc
hsz3855@gmail.com <hsz3855@gmail.com>
2021-01-03 22:06:18 +0900

    숨바꼭질 6

---
+++ BaekJoon/17087번_숨바꼭질 6.java
@@ -0,0 +1,52 @@
+﻿import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		
+		// 입출력
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		String ns = bf.readLine();
+		int n = Integer.valueOf(ns.split(" ")[1]);
+		String[] a = bf.readLine().split(" ");
+		int[] distance = new int[a.length];
+		
+		for(int i=0; i<a.length; i++) {
+			if( n> Integer.valueOf(a[i]) ) distance[i] = n-Integer.valueOf(a[i]);
+			else distance[i] = Integer.valueOf(a[i]) - n;
+		}
+		
+		int b = distance[0];
+		for(int i=1; i<distance.length; i++) {
+			b = gcd(b,distance[i]);
+			
+		}
+		
+		bw.write(b + "\n");
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	public static int  gcd(int a , int b) {
+		
+		while(true) {
+			int temp_b = b;
+			b = a%b;
+			a = temp_b;
+			if(b == 0) return a;
+		}		
+		
+	}
+	
+	
+
+		
+}
