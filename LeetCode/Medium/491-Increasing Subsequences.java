db8ca5fd6b3eeba1403ac1cd9c876e692a13fa04
SooJong <46212112+92SooJong@users.noreply.github.com>
2022-04-19 21:01:57 +0900

    Create 491-Increasing Subsequences.java

---
+++ LeetCode/Medium/491-Increasing Subsequences.java
@@ -0,0 +1,43 @@
+/**
+ 정답의 중복을 제거하기 위해 ArrayList의 contains를 사용했으나 시간초과 발생
+ Set을 사용해서 중복된 데이터가 보관되지 않도록 함.
+ 해결을 위해 HashSet을 사용했는데 submit을 할때마다 시간이 일정하지 않음.
+*/
+
+class Solution {
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        List<List<Integer>> result= new ArrayList<>();
+        HashSet<ArrayList<Integer>> tempResult= new HashSet<>();
+        
+        for(int i=0; i<nums.length; i++){
+
+
+            HashSet<ArrayList<Integer>> clone = new HashSet<>(tempResult);
+            // 선택한다
+            for (ArrayList<Integer> integers : clone) {
+                if(integers.get(integers.size()-1) <= nums[i]){
+                    ArrayList<Integer> clone1 = new ArrayList<>(integers);
+                    clone1.add(nums[i]);
+                    tempResult.add(clone1);
+
+                }
+            }
+
+            ArrayList<Integer> objects = new ArrayList<>();
+            objects.add(nums[i]);
+            tempResult.add(objects);
+
+
+        }
+
+
+        for (List<Integer> integers : tempResult) {
+            if(integers.size()>1){
+                result.add(integers);
+            }
+        }
+
+        return result;
+    }
+}
